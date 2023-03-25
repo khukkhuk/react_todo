2540ca3df0193b0f5636053ff0330f812c35dcb3
khukkhuk <khuk@hotmail.co.th>
2023-03-26 00:33:56 +0700

    First Commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import Header from './components/Header'
+import Form from './components/Form'
+import Todo from './components/Todo'
+import TodoList from './components/TodoList'
+import {useState} from 'react'
+function App() {
+  
+  const [todo,setTodo] = useState('')
+  const [todoList, setTodoList] = useState([])
+
+  return (
+    <div className="App">
+      <Header></Header>
+      <Form 
+        todo ={todo} 
+        setTodo ={setTodo} 
+        todoList ={todoList} 
+        setTodoList = {setTodoList}>
+      </Form>
+      <TodoList
+        todoList={todoList}
+        setTodoList ={setTodoList}
+      />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/TodoList.js
@@ -0,0 +1,18 @@
+import Todo from './Todo'
+const TodoList = ({todoList , setTodoList}) =>{
+
+    return(
+        <div>
+            {todoList.map((todoItem)=>(
+                <Todo 
+                    key={todoItem.id} 
+                    todoItem={todoItem} 
+                    todoList={todoList}
+                    setTodoList={setTodoList }
+                ></Todo>
+            ))}
+        </div>
+    )
+}
+
+export default TodoList;
